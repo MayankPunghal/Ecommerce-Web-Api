7bc769f4b866e9d3f185f317a46c5a271705594f
Mayank.Punghal <Mayank.Punghal@smartenergywater.in>
2024-03-10 22:21:36 +0530

    Fixed naming convention of images.

--- EcomApi.Utils/Common/ImageUtil.cs
+++ EcomApi.Utils/Common/ImageUtil.cs
@@ -5,7 +5,7 @@
         public static async Task<string> SaveImage(IFormFile File)
         {
             string ImageName = new String(Path.GetFileNameWithoutExtension(File.FileName).Take(10).ToArray()).Replace(" ", "-");
-            ImageName = ImageName + DateTime.Now.ToString("ttmmssfff") + Path.GetExtension(File.FileName);
+            ImageName = ImageName + DateTime.Now.ToString("yyMMddhhmmssfff") + Path.GetExtension(File.FileName);
             var ImagePath = AppSettings.Settings.CommonSettings.SharedFolderPath + ImageName;
             using (var fileStream = new FileStream(ImagePath, FileMode.Create))
             {
